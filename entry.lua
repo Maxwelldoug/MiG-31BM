54bd9c9ba196fae391ebbb20df0b4dbf8e8b90a1
Natalie <87938399+StuntPuppy@users.noreply.github.com>
2021-11-23 13:35:02 -0700

    Input fix attempt

--- entry.lua
+++ entry.lua
@@ -1,16 +1,17 @@
-declare_plugin("MiG-31BM",
+local self_ID = "MiG-31BM"
+declare_plugin(self_ID,
 {
-displayName     = _("MiG-31BM Foxhound "),
-developerName   =   "Eagle Dynamics",
+displayName     = _("MiG-31M/BM Foxhound"),
+developerName   = _("Foxhound Gang"),
 
 image     	 = "FC.bmp",
 installed 	 = true, -- if false that will be place holder , or advertising
 dirName	  	 = current_mod_path,
-fileMenuName = _("MiG-31BM"),
+fileMenuName = _("MiG-31M/BM"),
 
-version		 = "1.5.6",
+version		 = "1.1",
 state		 = "installed",
-info		 = _("MiG-31BM Foxhound. Originally Edited by Robert, Updated and repaired by WoodenPlankGames and StuntPuppy"),
+info		 = _("MiG-31M/BM Foxhound. Originally Edited by Robert, Updated and repaired by WoodenPlankGames and StuntPuppy"),
 
 Skins	=
 	{
@@ -18,6 +19,13 @@ Skins	=
 		    name	= _("MiG-31BM"),
 			dir		= "Theme"
 		},
+	},
+	Missions =
+	{
+		{
+			name		    = _("MiG-31BM"),
+			dir			    = "Missions",
+  		},
 	},
 LogBook =
 	{
@@ -28,7 +36,7 @@ LogBook =
 	},	
 InputProfiles =
 	{
-		["mig-31bm"] = current_mod_path .. '/Input',
+		["MiG-31BM"] = current_mod_path .. '/Input/MiG-31BM/',
 	},
 })
 mount_vfs_texture_path(current_mod_path.."/Theme/ME")--for simulator loading window
@@ -37,7 +45,7 @@ mount_vfs_model_path    (current_mod_path.."/Cockpit/Shape")
 mount_vfs_texture_path  (current_mod_path.."/Cockpit/Textures/MiG-31-CPT-TEXTURES")
 mount_vfs_liveries_path (current_mod_path.."/Liveries")
 ----------------------------------------------------------------------------------------
-make_flyable('MiG-31', current_mod_path..'/Cockpit/KneeboardRight/',{nil, old = 4}, current_mod_path..'/Entry/comm.lua')
+make_flyable('MiG-31BM', current_mod_path..'/Cockpit/KneeboardRight/',{nil, old = 4}, current_mod_path..'/Entry/comm.lua')
 ----------------------------------------------------------------------------------------
 dofile(current_mod_path.."/Entry/Views.lua")
 dofile(current_mod_path.."/Entry/MiG-31BM.lua")
