d553dc84e9b52ee59674bd475d8cf39671d14024
Maxwelldoug <maxmacneill@hotmail.ca>
2022-01-18 13:59:25 -0400

    Input Fix Take 1

--- entry.lua
+++ entry.lua
@@ -36,7 +36,7 @@ LogBook =
 	},	
 InputProfiles =
 	{
-		["MiG-31BM"] = current_mod_path .. '/Input/MiG-31BM',
+		["MiG-31BM"] = current_mod_path .. '/Input',
 	},
 })
 mount_vfs_texture_path(current_mod_path.."/Theme/ME")--for simulator loading window
